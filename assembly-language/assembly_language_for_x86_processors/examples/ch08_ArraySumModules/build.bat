2279db6171a1d4c17dbb39fb75c9c7b13b185ed3
Dixie Flatline <vitalisbox@gmail.com>
2018-09-17 20:00:53 +0300

    Change of purpose

--- assembly-language/assembly_language_for_x86_processors/examples/ch08_ArraySumModules/build.bat
+++
@@ -1,8 +0,0 @@
-PATH c:\Masm615
-
-SET INCLUDE=c:\Masm615\include
-SET LIB=c:\Masm615\lib
-
-ML -Zi -c -Fl -coff main.asm _display.asm _arraysum.asm _prompt.asm
-ML -Zi -c -Fl -coff _prompt.asm
-LINK32 main.obj _display.obj _arraysum.obj _prompt.obj irvine32.lib kernel32.lib /SUBSYSTEM:CONSOLE
